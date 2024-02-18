b81e1ec4366b90d5c1f9befad3eaa66a04f982d8
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-02-18 12:14:23 -0500

    Update Category model with description field

--- auctions/models.py
+++ auctions/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
 
 class Category(models.Model):
     category = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
     
     def __str__(self):
         return f"{self.category}"
