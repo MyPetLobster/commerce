013ef5c7aa4c0156ed41abcd35fad5f3f51f9b17
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-02-18 19:03:30 -0500

    Install celery, redis and configure for app

--- commerce/settings.py
+++ commerce/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
