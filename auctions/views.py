2f329e30f426be622c2a403a2034a6d3c80ec315
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-02-22 11:50:39 -0500

    Create listing first pass css
    Customize widgets ListingForm, use form-control

--- auctions/views.py
+++ auctions/views.py
@@ -1,6 +1,7 @@
 import math
 from datetime import datetime, timezone
 
+from django import forms
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -18,6 +19,13 @@ class ListingForm(ModelForm):
     class Meta:
         model = Listing
         fields = ['title', 'description', 'price', 'image', 'categories']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image': forms.URLInput(attrs={'class': 'form-control'}),
+            'categories': forms.CheckboxSelectMultiple(),
+        }
 
 class CommentForm(ModelForm):
     class Meta:
