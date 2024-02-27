dd3e27864ddf523ec1cb2e5eaf87ea2b3e321076
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-02-27 15:23:29 -0500

    Add celery beat config to celery.py

--- commerce/celery.py
+++ commerce/celery.py
@@ -6,3 +6,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 app = Celery('your_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# Configure celery beat schedule
+app.conf.beat_schedule = {
+    'check-listing-expiration': {
+        'task': 'auctions.tasks.check_listing_expiration',
+        'schedule': 60.0,
+    },
+}
