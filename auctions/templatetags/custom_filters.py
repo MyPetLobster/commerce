c1c607f5a730931c84bdd08087c55759d0e72f0b
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-03-03 19:50:34 -0500

    Create basic color classes, modify filter
    - get_time_left returns two values, time and status.
    
     Status used in span class value on watchlist.html

--- auctions/templatetags/custom_filters.py
+++ auctions/templatetags/custom_filters.py
@@ -1,3 +1,5 @@
+
+from django.utils import timezone
 from django import template
 from auctions.models import User, Bid, Listing
 
@@ -17,3 +19,18 @@ def get_user_high_bid(value, user_id,):
         return f'${bids.order_by('-amount').first().amount}'
     else:
         return 'No Bids'
+    
+
+@register.filter
+def get_time_left(listing_id):
+    listing = Listing.objects.get(pk=listing_id)
+    now = timezone.now()
+    time_left = listing.date + timezone.timedelta(days=7) - now
+    if time_left.days < 1:
+        status = 'red'
+    elif time_left.days < 2:
+        status = 'yellow'
+    else:
+        status = 'green'
+    time_left = f'{time_left.days} days, {time_left.seconds//3600} hours, {(time_left.seconds//60)%60} minutes left'
+    return time_left, status
