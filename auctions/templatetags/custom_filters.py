a73049980db83b7c033357f198af76f0920b4ad0
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-03-03 19:21:14 -0500

    Create new custom filter to get usr high bid
    - use new function to display in watchlist

--- auctions/templatetags/custom_filters.py
+++ auctions/templatetags/custom_filters.py
@@ -1,7 +1,19 @@
 from django import template
+from auctions.models import User, Bid, Listing
 
 register = template.Library()
 
 @register.filter
 def subtract(value, arg):
     return value - arg
+
+
+@register.filter
+def get_user_high_bid(value, user_id,):
+    user = User.objects.get(pk=user_id)
+    listing = Listing.objects.get(pk=value)
+    bids = Bid.objects.filter(listing=listing, user=user)
+    if bids.exists():
+        return f'${bids.order_by('-amount').first().amount}'
+    else:
+        return 'No Bids'
