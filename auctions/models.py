1c73194001eac95739c451b46d242f2cea42f9ac
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-03-09 12:08:06 -0500

    Reorder models.py classes
    - add new field 'note' Transactions model

--- auctions/models.py
+++ auctions/models.py
@@ -2,8 +2,38 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
+
 class User(AbstractUser):
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.category}"
+    
+
+class Listing(models.Model):
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    active = models.BooleanField(default=True)
+    in_escrow = models.BooleanField(default=False)
+    shipped = models.BooleanField(default=False)
+    cancelled = models.BooleanField(default=False)
+    categories = models.ManyToManyField(Category, related_name="listings")
+    description = models.TextField()
+    image = models.URLField(blank=True)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=64)
+    closing_date = models.DateTimeField(blank=True, null=True)
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="won_listings", null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.title} - {self.price}"
 
 
 class Transaction(models.Model):
@@ -12,6 +42,7 @@ class Transaction(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_transactions")
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_transactions")
     listing = models.ForeignKey('Listing', on_delete=models.CASCADE, blank=True, null=True, related_name="transactions")
+    note = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.sender} - {self.recipient} - {self.amount}"
@@ -29,38 +60,10 @@ class Message(models.Model):
     def __str__(self):
         return f"{self.sender} - {self.recipient} - {self.message}"
         
-
-class Category(models.Model):
-    category = models.CharField(max_length=64)
-    description = models.TextField(blank=True)
-    
-    def __str__(self):
-        return f"{self.category}"
-
-
-class Listing(models.Model):
-    active = models.BooleanField(default=True)
-    in_escrow = models.BooleanField(default=False)
-    shipped = models.BooleanField(default=False)
-    cancelled = models.BooleanField(default=False)
-    categories = models.ManyToManyField(Category, related_name="listings")
-    date = models.DateTimeField()
-    description = models.TextField()
-    image = models.URLField(blank=True)
-    starting_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    title = models.CharField(max_length=64)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    closing_date = models.DateTimeField(blank=True, null=True)
-    winner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="won_listings", null=True, blank=True)
-    
-    def __str__(self):
-        return f"{self.title} - {self.price}"
-    
     
 class Bid(models.Model):
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     
@@ -69,9 +72,9 @@ class Bid(models.Model):
     
 
 class Comment(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
     anonymous = models.BooleanField(default=False)
     comment = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
 
@@ -85,15 +88,3 @@ class Watchlist(models.Model):
     
     def __str__(self):
         return f"{self.user} - {self.listing}"
-    
-
-# class Winner(models.Model):
-#     amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     date = models.DateTimeField(auto_now_add=True)
-#     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="winner")
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="winner")
-    
-#     def __str__(self):
-#         return f"{self.user} - {self.listing}"
-
-
