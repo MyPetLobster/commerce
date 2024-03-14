afac30ab858e94a74cdd0aa6157b8b88bec2f50f
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-03-14 11:19:08 -0400

    Refactor expand.js, remove vis class, organize

--- auctions/static/auctions/javascript/expand.js
+++ auctions/static/auctions/javascript/expand.js
@@ -1,17 +1,18 @@
-const showFullListings = document.querySelector(".full-listings")
-const showTitleOnly = document.querySelector(".title-only")
-
-const shortListings = document.querySelectorAll(".short-listings")
-const longListings = document.querySelectorAll(".long-listings")
-
+// LOCAL STORAGE FOR FULL OR TITLE ONLY LISTINGS
 var fullOrTitle = localStorage.getItem("fullOrTitle")
 
+// SHOW FULL OR TITLE ONLY LISTINGS
+// Buttons to show full or title only listings
+const showFullListings = document.querySelector(".full-listings")
+const showTitleOnly = document.querySelector(".title-only")
+// Full and short listings
+const shortListings = document.querySelectorAll(".short-listings")
+const longListings = document.querySelectorAll(".long-listings")
 
 showFullListings.addEventListener("click", () => {
     showFull();
     initializeShowMoreBasedOnLocalStorage();
 });
-
 showTitleOnly.addEventListener("click", () => {
     showTitle();
     initializeShowMoreBasedOnLocalStorage();
@@ -26,11 +27,9 @@ function showFull() {
 
   shortListings.forEach((listing) => {
     listing.classList.add("hidden");
-    listing.classList.remove("visible");
   });
 
   longListings.forEach((listing) => {
-    listing.classList.add("visible");
     listing.classList.remove("hidden");
   });
 
@@ -45,11 +44,9 @@ function showTitle() {
 
   longListings.forEach((listing) => {
     listing.classList.add("hidden");
-    listing.classList.remove("visible");
   });
 
   shortListings.forEach((listing) => {
-    listing.classList.add("visible");
     listing.classList.remove("hidden");
   });
 
@@ -57,18 +54,16 @@ function showTitle() {
 }
 
 
+// TRUNCATE LISTING DESCRIPTIONS
 // check length of the description, if over 125 characters, truncate and add a link to expand
 var allDescriptions = document.querySelectorAll(".full-description")
 var allTruncatedDescriptions = document.querySelectorAll(".truncated-description")
 for (var i = 0; i < allDescriptions.length; i++) {
     if (allDescriptions[i].innerText.length > 200) {
         allDescriptions[i].classList.add("hidden")
-        allDescriptions[i].classList.remove("visible")
-        allTruncatedDescriptions[i].classList.add("visible")
         allTruncatedDescriptions[i].classList.remove("hidden")
     }
 }
-
 // add event listener to expand the description
 document.querySelectorAll(".expand-link").forEach((link) => {
     link.addEventListener("click", () => {
@@ -77,23 +72,17 @@ document.querySelectorAll(".expand-link").forEach((link) => {
         
         link.parentElement.previousElementSibling.classList.add("pointer")    
         link.parentElement.previousElementSibling.classList.remove("hidden")
-        link.parentElement.previousElementSibling.classList.add("visible")
 
         link.parentElement.previousElementSibling.addEventListener("click", () => {
             link.parentElement.previousElementSibling.classList.add("hidden")
-            link.parentElement.previousElementSibling.classList.remove("visible")
             link.parentElement.previousElementSibling.classList.remove("pointer")
             link.parentElement.classList.remove("hidden")
-            link.parentElement.classList.add("visible")
         });
     });
 });
 
 
-
-
 // NUMBER OF LISTINGS TO DISPLAY
-
 // numberOfListings will control how many listings are displayed at a time across index and listings pages
 const numberOfListings = 10;
 
@@ -139,7 +128,9 @@ function initializeActiveListings() {
 function initializeClosedListings() {
     const allClosedListings = document.querySelectorAll('.li-listing-closed-div');
     const showMoreClosed = document.getElementById('show-more-closed');
-    initializeShowMore(allClosedListings, showMoreClosed, allClosedListings.length);
+    if (allClosedListings.length > 10) {
+        initializeShowMore(allClosedListings, showMoreClosed, allClosedListings.length);
+    }
 }
 
 // Function to check and initialize based on localStorage value
@@ -153,10 +144,8 @@ function initializeShowMoreBasedOnLocalStorage() {
     }
 }
 
-// Initialize show more functionality
-initializeShowMoreBasedOnLocalStorage();
 
-
+// INITIALIZE BASED ON LOCAL STORAGE
 document.addEventListener("DOMContentLoaded", () => {
   if (fullOrTitle === "full") {
     showFull();
