89cbeb26244f4a5e479b40b8c71e36cdd6786ede
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-03-15 11:19:20 -0400

    Setup celery beat and schedule tasks

--- auctions/tasks.py
+++ auctions/tasks.py
@@ -16,58 +16,7 @@ logger = logging.getLogger(__name__)
 
 
 # Periodic Tasks
-# Used in - views.index
-@shared_task
-def check_if_bids_funded():
-    '''
-    This task checks if bids on active listings are funded. If there are less than 24 hours left before the listing closes
-    and the bid is not funded, the bid is deleted and the user is notified.
-
-    Frequency: Every 60 minutes
-
-    Args: None
-    Returns: None
-
-    Called by: views.index
-    '''
-    # Retrieve all the active bids that are closing within the next 24 hours
-    active_bids = Bid.objects.filter(listing__active=True,
-                                      listing__closing_date__gt=timezone.now(),
-                                      listing__closing_date__lt=timezone.now() + timezone.timedelta(days=1))
-
-    # Dictionary to hold users and listings so we only message each user once for each unique listing
-    # Even if we are deleting multiple bids they have placed
-    messaged_users_listings = defaultdict(int)
-
-    for bid in active_bids:
-        user = bid.user
-        listing = bid.listing
-
-        # Update the listing.price to highest bid each iteration
-        highest_bid = Bid.objects.filter(listing=listing).order_by('-amount').first()
-        listing.price = highest_bid.amount
-        listing.save()
-
-        now = timezone.now()
-        listing_closing_date = listing.closing_date
-
-        # Get cutoff date for unfunded bids, 1 day before listing closing date
-        cutoff_date = listing_closing_date - timezone.timedelta(days=1)
-
-        # If the bid is unfunded and the listing is closing within the next 24 hours, delete the bid and notify the user
-        if now > cutoff_date and user.balance < listing.price:
-            bid.delete()
-
-            time_left = listing_closing_date - now
-            hours_left = time_left.total_seconds() // 3600
-            minutes_left = (time_left.total_seconds() % 3600) // 60
-            time_left_str = f"{int(hours_left)} hours and {int(minutes_left)} minutes"
-
-            if not messaged_users_listings[(user.id, listing.id)]:
-                a_msg.send_bid_removed_message(user, listing, time_left_str)
-                messaged_users_listings[(user.id, listing.id)] = 1
-        
-
+# Used in - views.index, celery.py
 @shared_task
 def set_inactive():
     '''
@@ -157,3 +106,52 @@ def check_user_fees():
             # TODO: Send email to user to notify them and give contact info for collections department
 
 
+@shared_task
+def check_if_bids_funded():
+    '''
+    This task checks if bids on active listings are funded. If there are less than 24 hours left before the listing closes
+    and the bid is not funded, the bid is deleted and the user is notified.
+
+    Frequency: Every 60 minutes
+
+    Args: None
+    Returns: None
+
+    Called by: views.index
+    '''
+    # Retrieve all the active bids that are closing within the next 24 hours
+    active_bids = Bid.objects.filter(listing__active=True,
+                                      listing__closing_date__gt=timezone.now(),
+                                      listing__closing_date__lt=timezone.now() + timezone.timedelta(days=1))
+
+    # Dictionary to hold users and listings so we only message each user once for each unique listing
+    # Even if we are deleting multiple bids they have placed
+    messaged_users_listings = defaultdict(int)
+
+    for bid in active_bids:
+        user = bid.user
+        listing = bid.listing
+
+        # Update the listing.price to highest bid each iteration
+        highest_bid = Bid.objects.filter(listing=listing).order_by('-amount').first()
+        listing.price = highest_bid.amount
+        listing.save()
+
+        now = timezone.now()
+        listing_closing_date = listing.closing_date
+
+        # Get cutoff date for unfunded bids, 1 day before listing closing date
+        cutoff_date = listing_closing_date - timezone.timedelta(days=1)
+
+        # If the bid is unfunded and the listing is closing within the next 24 hours, delete the bid and notify the user
+        if now > cutoff_date and user.balance < listing.price:
+            bid.delete()
+
+            time_left = listing_closing_date - now
+            hours_left = time_left.total_seconds() // 3600
+            minutes_left = (time_left.total_seconds() % 3600) // 60
+            time_left_str = f"{int(hours_left)} hours and {int(minutes_left)} minutes"
+
+            if not messaged_users_listings[(user.id, listing.id)]:
+                a_msg.send_bid_removed_message(user, listing, time_left_str)
+                messaged_users_listings[(user.id, listing.id)] = 1
--- commerce/celery.py
+++ commerce/celery.py
@@ -2,15 +2,41 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
-app = Celery('your_project')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
+app = Celery('commerce')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+
 # Configure celery beat schedule
 app.conf.beat_schedule = {
-    'check-listing-expiration': {
-        'task': 'auctions.tasks.check_listing_expiration',
+    'set_inactive': {
+        'task': 'auctions.tasks.set_inactive',
         'schedule': 60.0,
     },
+    'check_user_fees': {
+        'task': 'auctions.tasks.check_user_fees',
+        'schedule': 43200.0,
+    },
+    'check_if_bids_funded': {
+        'task': 'auctions.tasks.check_if_bids_funded',
+        'schedule': 3600.0,
+    },
 }
+
+
+# TO RUN:
+
+# start redis server
+# redis-server
+
+# Run celery worker
+# celery -A commerce worker -l info
+
+# Run celery beat
+# celery -A commerce beat -l info
+
+
+
+
